8d244fbe98c09a4b0d7c5eec3a9abcbdf7fc4f3b
zhichkin <dima_zhichkin@mail.ru>
2021-03-26 01:00:27 +0300

    Эксперименты с парсингом

---
+++ src/dajet-metadata-tests/NewParser.cs
@@ -0,0 +1,45 @@
+﻿using DaJet.Metadata.Parsers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Text;
+
+namespace DaJet.Metadata.Tests
+{
+    [TestClass] public sealed class NewParser
+    {
+        [TestMethod] public void TestNewParser()
+        {
+            MDObject mdObject;
+            string filePath = @"C:\temp\Контрагенты.txt"; //@"C:\temp\my_exchange._Enum89.txt";
+            using (StreamReader stream = new StreamReader(filePath, Encoding.UTF8))
+            {
+                mdObject = MDObjectParser.Parse(stream);
+            }
+            using (StreamWriter stream = new StreamWriter(@"C:\temp\output.txt", false, Encoding.UTF8))
+            {
+                ShowMDObject(stream, mdObject, 0);
+            }
+        }
+        private void ShowMDObject(StreamWriter stream, MDObject mdObject, int level)
+        {
+            string indent = level == 0 ? string.Empty : "-".PadLeft(level * 4, '-');
+            foreach (object value in mdObject.Values)
+            {
+                if (value is MDObject child)
+                {
+                    stream.WriteLine(indent + "[" + level.ToString() + "]" + " " + value.ToString());
+                    ShowMDObject(stream, child, level + 1);
+                }
+                else if (value is string text)
+                {
+                    stream.WriteLine(indent + "[" + level.ToString() + "]" + " \"" + text.ToString() + "\"");
+                }
+                else
+                {
+                    stream.WriteLine(indent + "[" + level.ToString() + "]" + " " + value.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ src/dajet-metadata/parsers/MDObjectParser.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System;
+
+namespace DaJet.Metadata.Parsers
+{
+    public sealed class MDObject
+    {
+        public List<object> Values { get; } = new List<object>();
+    }
+    public static class MDObjectParser
+    {
+        public static MDObject Parse(StreamReader stream)
+        {
+            MDObject mdObject = new MDObject();
+            ParseObject(stream, mdObject, 0);
+            return mdObject;
+        }
+        private static void ParseObject(StreamReader stream, MDObject parent, int level)
+        {
+            char c;
+            string value = null;
+            while (!stream.EndOfStream)
+            {
+                do
+                {
+                    c = (char)stream.Read();
+                }
+                while (c == '\r' || c == '\n');
+
+                if (c == '{') // start of object
+                {
+                    if (level == 0) // this is root object
+                    {
+                        ParseObject(stream, parent, level + 1);
+                    }
+                    else
+                    {
+                        MDObject child = new MDObject();
+                        ParseObject(stream, child, level + 1);
+                        parent.Values.Add(child);
+                    }
+                }
+                else if (c == '}') // end of object
+                {
+                    if (value != null)
+                    {
+                        parent.Values.Add(value);
+                    }
+                    return; 
+                }
+                else if (c == ',') // end of value
+                {
+                    if (value != null)
+                    {
+                        parent.Values.Add(value);
+                        value = null;
+                    }
+                }
+                else if (c == '"') // start of string value
+                {
+                    value = string.Empty;
+                    while (!stream.EndOfStream)
+                    {
+                        c = (char)stream.Read();
+                        if (c == '"') // might be end of string
+                        {
+                            c = (char)stream.Read();
+                            if (c == '"') // double quotes - this is not the end
+                            {
+                                value += c;
+                            }
+                            else // this is the end
+                            {
+                                parent.Values.Add(value);
+                                value = null;
+                                if (c == '}') // end of object
+                                {
+                                    return;
+                                }
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            value += c;
+                        }
+                    }
+                }
+                else // number or uuid value
+                {
+                    if (value == null)
+                    {
+                        value = c.ToString();
+                    }
+                    else
+                    {
+                        value += c;
+                    }
+                }
+            }
+        }
+    }
+}
