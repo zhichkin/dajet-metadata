c3973f4878b3a86e03c1513b0af9540f9c096112
zhichkin <dima_zhichkin@mail.ru>
2022-06-13 01:45:00 +0300

    dajet-gen-view 1.0.0

--- doc/sql-code-generator/README.md
+++ doc/sql-code-generator/README.md
@@ -1,6 +1,8 @@
 ## DaJet SQL views generator
 
-Требуется установка [.NET 6.0](https://dotnet.microsoft.com/download/).
+Требуется установка [.NET 6.0](https://dotnet.microsoft.com/download/)
+
+[NuGet](https://www.nuget.org/packages/DaJet.CodeGenerator) & [Telegram](https://t.me/dajet_studio_group)
 
 Утилита **dajet-gen-view** создаёт представления СУБД (view)
 для объектов конфигруации 1С:Предприятие 8.
@@ -8,7 +10,8 @@
 На данный момент времени поддерживается только Microsoft SQL Server.
 
 Представления создаются в той же самой базе данных,
-в которой расположены сами объекты.
+в которой расположены объекты 1С:Предприятие 8.
+При совпадении имён представления пересоздаются.
 
 Это позволяет обращаться к данным объектов непосредственно
 при помощи языка запросов СУБД по именам этих объектов,
@@ -41,201 +44,71 @@ SELECT Порядок, Имя, Синоним, Значение FROM [Переч
 
 ### Создание представлений СУБД
 
-**dajet-gen-view.exe** create --ms "Data Source=one_c_sql_server;Initial Catalog=one_c_database;Integrated Security=True;Encrypt=False;"
+**dajet-gen-view.exe** **create** --ms "Data Source=one_c_sql_server;Initial Catalog=one_c_database;Integrated Security=True;Encrypt=False;"
 
 ### Удаление представлений СУБД
 
-**dajet-gen-view.exe** delete --ms "Data Source=one_c_sql_server;Initial Catalog=one_c_database;Integrated Security=True;Encrypt=False;"
+**dajet-gen-view.exe** **delete** --ms "Data Source=one_c_sql_server;Initial Catalog=one_c_database;Integrated Security=True;Encrypt=False;"
 
-[NuGet package](https://www.nuget.org/packages/DaJet.Metadata/) & [Telegram support channel](https://t.me/dajet_studio_group)
-
-[Конфигурация 1С для выполнения тестов](https://github.com/zhichkin/dajet-metadata/blob/main/1c/dajet-metadata-test-base.cf)
-
-Кроме метаданных 1С дополнительно можно выполнять чтение метаданных СУБД.
-
-<details>
-<summary>Пример загрузки метаданных и свойств конфигурации</summary>
+### Программное создание представлений СУБД
 
 ```C#
+using DaJet.CodeGenerator.SqlServer;
 using DaJet.Metadata;
 using DaJet.Metadata.Model;
 
 static void Main(string[] args)
 {
-    string csPostgres = "Host=127.0.0.1;Port=5432;Database=MY_1C_DATABASE;Username=postgres;Password=postgres;";
-    string csSqlServer = "Data Source=MY_DATABASE_SERVER;Initial Catalog=MY_1C_DATABASE;Integrated Security=True";
-
     IMetadataService metadataService = new MetadataService();
 
-    #region "PostgreSQL"
-
-    // Настройки для подключения к PostgreSQL
-    metadataService.
-        .UseConnectionString(csPostgres)
-        .UseDatabaseProvider(DatabaseProviders.PostgreSQL);
-
-    // 1. Пример чтения метаданных конфигурации 1С
-    InfoBase infoBase = metadataService.LoadInfoBase();
-    
-    // 2. Пример чтения свойств конфигурации 1С
-    ConfigInfo config = metadataService.ReadConfigurationProperties();
-
-    // 3. Пример дополнения справочника "Номенклатура" свойствами из базы данных
-    MetadataObject catalog = infoBase.Catalogs.Values
-                                 .Where(i => i.Name == "Номенклатура")
-                                 .FirstOrDefault();
-
-    if (catalog == null)
+    if (!metadataService
+        .UseDatabaseProvider(DatabaseProvider.SQLServer)
+        .UseConnectionString("Data Source=SQL_SERVER;Initial Catalog=MY_DATABASE;Integrated Security=True;Encrypt=False;")
+        .TryOpenInfoBase(out InfoBase infoBase, out string message))
     {
-        Console.WriteLine("Справочник \"Номенклатура\" не найден!");
+        Console.WriteLine("Error: " + message);
         return;
     }
 
-    Console.WriteLine("Свойства справочника до обогащения из базы данных.");
-    Console.WriteLine(catalog.Name + " (" + catalog.TableName + "):");
-    foreach (MetadataProperty property in catalog.Properties)
+    SqlGeneratorOptions options = new SqlGeneratorOptions()
     {
-        Console.WriteLine(" - " + property.Name + " (" + property.DbName + ")");
+        DatabaseProvider = "SqlServer",
+        ConnectionString = metadataService.ConnectionString
+    };
+
+    ISqlGenerator generator = new SqlGenerator(options);
+
+    if (!generator.TryCreateViews(in infoBase, out int result, out List<string> errors))
+    {
+        foreach (string error in errors)
+        {
+            Console.WriteLine(error);
+        }
     }
 
-    // Выполняем сравнение и объединение с метаданными СУБД
-    metadataService.EnrichFromDatabase(catalog);
-
-    Console.WriteLine("Свойства справочника после обогащения из базы данных.");
-    Console.WriteLine(catalog.Name + " (" + catalog.TableName + "):");
-    foreach (MetadataProperty property in catalog.Properties)
-    {
-        Console.WriteLine(" - " + property.Name + " (" + property.DbName + ")");
-    }
-
-    #endregion
-
-    #region "Microsoft SQL Server"
-
-    // Настройки для подключения к Microsoft SQL Server
-    metadataService.
-        .UseConnectionString(csSqlServer)
-        .UseDatabaseProvider(DatabaseProviders.SQLServer);
-
-    // Всё остальное работает ровно также, как и для PostgreSQL
-
-    #endregion
+    Console.WriteLine($"Created {result} views");
 }
 ```
 
-</details>
-
-<details>
-<summary>Пример загрузки свойств и узлов плана обмена</summary>
+### Программное удаление представлений СУБД
 
 ```C#
-using DaJet.Metadata;
-using DaJet.Metadata.Model;
-using DaJet.Metadata.Mappers;
-
-static void Main(string[] args)
-{
-    string connectionString = "Data Source=MY_DATABASE_SERVER;Initial Catalog=MY_1C_DATABASE;Integrated Security=True";
-
-    IMetadataService metadataService = new MetadataService();
-
-    metadataService.
-        .UseConnectionString(connectionString)
-        .UseDatabaseProvider(DatabaseProviders.SQLServer);
-
-    InfoBase infoBase = metadataService.LoadInfoBase();
-
-    // Находим план обмена (публикацию) по его имени, как оно указано в конфигурации 1С
-    Publication publication = infoBase.Publications.Values
-                                  .Where(i => i.Name == "ТестовыйПланОбмена")
-                                  .FirstOrDefault();
-
-    if (publication == null)
-    {
-        Console.WriteLine("План обмена \"ТестовыйПланОбмена\" не найден!");
-        return;
-    }
-
-    // Создаём экземпляр класса для загрузки данных плана обмена
-    PublicationDataMapper mapper = new PublicationDataMapper();
-    mapper.UseDatabaseProvider(metadataService.DatabaseProvider);
-    mapper.UseConnectionString(metadataService.ConnectionString);
-
-    // Загружаем узлы плана обмена (подписчиков)
-    mapper.SelectSubscribers(publication);
-
-    // Выводим информацию об "этом узле" (издателе)
-    // Код 1С: ПланыОбмена.ТестовыйПланОбмена.ЭтотУзел()
-    Console.WriteLine(string.Format("Publisher: ({0}) {1}",
-        publication.Publisher.Code,
-        publication.Publisher.Name));
-
-    // Выводим информацию об узлах плана обмена (подписчиках)
-    Console.WriteLine("Subscribers:");
-    foreach (Subscriber subscriber in publication.Subscribers)
-    {
-        Console.WriteLine(string.Format(" - ({0}) {1} [{2}]",
-            subscriber.Code,
-            subscriber.Name,
-            subscriber.IsMarkedForDeletion ? "x" : "+"));
-    }
-}
-```
-
-</details>
-
-<details>
-<summary>Пример сравнения метаданных СУБД для объекта метаданных 1С</summary>
-
-```C#
+using DaJet.CodeGenerator.SqlServer;
 using DaJet.Metadata;
 using DaJet.Metadata.Model;
 
 static void Main(string[] args)
 {
-    string connectionString = "Data Source=MY_DATABASE_SERVER;Initial Catalog=MY_1C_DATABASE;Integrated Security=True";
-
-    IMetadataService metadataService = new MetadataService();
-
-    metadataService.
-        .UseConnectionString(connectionString)
-        .UseDatabaseProvider(DatabaseProviders.SQLServer);
-
-    InfoBase infoBase = metadataService.LoadInfoBase();
-
-    // Находим объект метаданных 1С для загрузки его метаданных СУБД
-    Publication publication = infoBase.Publications.Values
-                                  .Where(i => i.Name == "ТестовыйПланОбмена")
-                                  .FirstOrDefault();
-
-    if (publication == null)
+    SqlGeneratorOptions options = new SqlGeneratorOptions()
     {
-        Console.WriteLine("План обмена \"ТестовыйПланОбмена\" не найден!");
-        return;
-    }
+        DatabaseProvider = "SqlServer",
+        ConnectionString = "Data Source=SQL_SERVER;Initial Catalog=MY_DATABASE;Integrated Security=True;Encrypt=False;"
+    };
 
-    List<string> delete_list; // Список "лишних" - есть в объекте метаданных 1С, но не найдены в базе данных
-    List<string> insert_list; // Список "новых" - есть в базе данных, но нет в объекте метаданных 1С
-    bool result = metadataService.CompareWithDatabase(publication, out delete_list, out insert_list);
+    ISqlGenerator generator = new SqlGenerator(options);
+
+    int result = generator.DropViews();
+
+    Console.WriteLine($"Dropped {result} views");
 }
 ```
-
-</details>
-
-**Утилита для чтения метаданных (свойств конфигурации)**
-
-[Ссылка для скачивания](https://github.com/zhichkin/dajet-metadata/releases/)
-
-Помощь по использованию:
-
-![Помощь по использованию](https://github.com/zhichkin/dajet-metadata/blob/main/doc/dajet-help.png)
-
-Пример использования для Microsoft SQL Server:
-
-![Пример использования dajet cli для Microsoft SQL Server](https://github.com/zhichkin/dajet-metadata/blob/main/doc/dajet-usage-ms.png)
-
-Пример использования для PostgreSQL:
-
-![Пример использования dajet cli для PostgreSQL](https://github.com/zhichkin/dajet-metadata/blob/main/doc/dajet-usage-pg.png)
-
-**Примечание:** в случае необходимости указать порт для **PostgreSQL** адрес сервера можно указать, например, так: **127.0.0.1:5432**
--- src/dajet-gen-view/Program.cs
+++ src/dajet-gen-view/Program.cs
@@ -1,220 +1,139 @@
-﻿namespace DaJet.CodeGenerator
+﻿using DaJet.CodeGenerator.SqlServer;
+using DaJet.Metadata;
+using DaJet.Metadata.Model;
+using System.CommandLine;
+using System.Diagnostics;
+
+namespace DaJet.CodeGenerator
 {
     public static class Program
     {
         public static int Main(string[] args)
         {
             //args = new string[] { "--help" };
+            //args = new string[] { "--version" };
 
-            //string ms = "Data Source=zhichkin;Initial Catalog=cerberus;Integrated Security=True";
-            //string pg = "Host=127.0.0.1;Port=5432;Database=test_node_2;Username=postgres;Password=postgres;";
-            //string rmq = "amqp://guest:guest@localhost:5672";
-            //string json = "C:\\temp\\export-data.json";
-            //args = new string[] { "export",
-            //    "--ms", ms,
-            //    "--rmq", rmq,
-            //    "--data", "Справочник.Клиенты",
-            //    "--page", "1-1",
-            //    "--route", "dajet-queue"
-            //};
+            //string ms = "Data Source=zhichkin;Initial Catalog=dajet-metadata-ms;Integrated Security=True;Encrypt=False;";
+            //args = new string[] { "create", "--ms", ms };
+            //args = new string[] { "delete", "--ms", ms };
 
-            RootCommand command = new RootCommand()
+            RootCommand command = new()
             {
-                Description = "DaJet Console 1.0"
+                Description = "DaJet database view generator 1.0.0"
             };
 
-            Command export = new Command("export")
-            {
-                new Option<string>("--ms",   "SQL Server connection string"),
-                new Option<string>("--pg",   "PostgreSQL connection string"),
-                new Option<string>("--rmq",  "RabbitMQ URI (amqp://guest:guest@localhost:5672/{vhost}/{exchange})"),
-                new Option<string>("--json", "JSON file full path"),
-                new Option<string>("--data", "Full name of 1С metadata object (Справочник.Номенклатура)"),
-                new Option<string>("--page", "Range of data pages to export (page size is 1000 rows)"),
-                new Option<string>("--route", "Routing key to use with RabbitMQ")
-            };
-            export.Description = "Export data from 1C database to RabbitMQ exchange or JSON file"
-                + Environment.NewLine + "Options:";
-            foreach (Option option in export.Options)
-            {
-                export.Description += Environment.NewLine + "  --" + option.Name.PadRight(6) + option.Description;
-            }
-            export.Handler = CommandHandler.Create<string, string, string, string, string, string, FileInfo>(ExecuteExportCommand);
-
-            command.Add(export);
+            command.Add(CreateCommandFactory());
+            
+            command.Add(DeleteCommandFactory());
 
             return command.Invoke(args);
         }
-        private static ApplicationObject GetApplicationObjectByName(InfoBase infoBase, string metadataName)
+        private static Command CreateCommandFactory()
         {
-            string[] names = metadataName.Split('.');
-            if (names.Length != 2)
+            var ms = new Option<string>("--ms", "SQL Server connection string");
+            var pg = new Option<string>("--pg", "PostgreSQL connection string");
+
+            Command create = new("create") { ms, pg };
+
+            create.Description = "Create database views"
+                + Environment.NewLine + "Options:";
+
+            foreach (Option option in create.Options)
             {
-                return null;
+                create.Description += Environment.NewLine + "  --" + option.Name.PadRight(4) + option.Description;
             }
 
-            string typeName = names[0];
-            string objectName = names[1];
+            create.SetHandler<string, string>(ExecuteCreateCommand, ms, pg);
 
-            ApplicationObject metaObject = null;
-            Dictionary<Guid, ApplicationObject> collection = null;
-
-            if (typeName == "Справочник") collection = infoBase.Catalogs;
-            else if (typeName == "Документ") collection = infoBase.Documents;
-            else if (typeName == "ПланОбмена") collection = infoBase.Publications;
-            else if (typeName == "РегистрСведений") collection = infoBase.InformationRegisters;
-            else if (typeName == "РегистрНакопления") collection = infoBase.AccumulationRegisters;
-            if (collection == null)
-            {
-                return null;
-            }
-
-            metaObject = collection.Values.Where(o => o.Name == objectName).FirstOrDefault();
-            if (metaObject == null)
-            {
-                return null;
-            }
-
-            return metaObject;
+            return create;
         }
-        private static void ExecuteExportCommand(string ms, string pg, string rmq, string data, string page, string route, FileInfo json)
+        private static Command DeleteCommandFactory()
         {
-            //Console.WriteLine("Export command parameters:");
-            //Console.WriteLine("- ms = " + (string.IsNullOrWhiteSpace(ms) ? string.Empty : ms));
-            //Console.WriteLine("- pg = " + (string.IsNullOrWhiteSpace(pg) ? string.Empty : pg));
-            //Console.WriteLine("- rmq = " + (string.IsNullOrWhiteSpace(rmq) ? string.Empty : rmq));
-            //Console.WriteLine("- json = " + (json == null ? string.Empty : json.FullName));
-            //Console.WriteLine("- data = " + (string.IsNullOrWhiteSpace(data) ? string.Empty : data));
-            //Console.WriteLine("- page = " + (string.IsNullOrWhiteSpace(page) ? string.Empty : page));
-            //Console.WriteLine("- route = " + (string.IsNullOrWhiteSpace(route) ? string.Empty : route));
-            //Console.WriteLine();
+            var ms = new Option<string>("--ms", "SQL Server connection string");
+            var pg = new Option<string>("--pg", "PostgreSQL connection string");
 
-            IMetadataService metadata = new MetadataService()
-                .UseConnectionString(ms)
-                .UseDatabaseProvider(DatabaseProvider.SQLServer);
+            Command create = new("delete") { ms, pg };
 
-            InfoBase infoBase = metadata.OpenInfoBase();
+            create.Description = "Delete database views"
+                + Environment.NewLine + "Options:";
 
-            ApplicationObject metaObject = GetApplicationObjectByName(infoBase, data);
-            if (metaObject == null)
+            foreach (Option option in create.Options)
             {
-                Console.WriteLine($"Object \"{data}\" is not found.");
+                create.Description += Environment.NewLine + "  --" + option.Name.PadRight(4) + option.Description;
+            }
+
+            create.SetHandler<string, string>(ExecuteDeleteCommand, ms, pg);
+
+            return create;
+        }
+        private static void ExecuteCreateCommand(string ms, string pg)
+        {
+            if (!string.IsNullOrWhiteSpace(pg))
+            {
+                Console.WriteLine("Sorry, PostgreSql support is under construction ... ");
                 return;
             }
 
-            ParsePageParameter(page, out int firstPage, out int lastPage);
+            IMetadataService metadataService = new MetadataService();
 
-            int pageNumber = firstPage;
-            if (lastPage == 0)
+            if (!metadataService
+                .UseDatabaseProvider(DatabaseProvider.SQLServer)
+                .UseConnectionString(ms)
+                .TryOpenInfoBase(out InfoBase infoBase, out string message))
             {
-                lastPage = pageNumber;
+                Console.WriteLine("Error: " + message);
+                return;
             }
 
-            if (data == "Справочник.Клиенты")
+            SqlGeneratorOptions options = new SqlGeneratorOptions()
             {
-                ExportCatalog(infoBase, metaObject, metadata, pageNumber, lastPage, rmq, route);
-            }
-            else if (data == "РегистрСведений.ОтветственныеЛицаАптек")
-            {
-                ExportRegister(infoBase, metaObject, metadata, pageNumber, lastPage, rmq, route);
-            }
-            else
-            {
-                Console.WriteLine($"Unknown object: \"{data}\".");
-            }
+                DatabaseProvider = "SqlServer",
+                ConnectionString = metadataService.ConnectionString
+            };
 
-            //foreach (int i in GetNumbers())
-            //{
-            //    Console.SetCursorPosition(0, Console.CursorTop);
-            //    Console.Write("i = " + i.ToString());
-            //}
-        }
-        //public static IEnumerable<int> GetNumbers()
-        //{
-        //    for (int i = 0; i < 10; i++)
-        //    {
-        //        yield return i;
-        //    }
-        //}
-        private static void ParsePageParameter(string page, out int firstPage, out int lastPage)
-        {
-            if (page.Contains('-'))
+            ISqlGenerator generator = new SqlGenerator(options);
+
+            Stopwatch watch = new();
+
+            watch.Start();
+
+            if (!generator.TryCreateViews(in infoBase, out int result, out List<string> errors))
             {
-                string[] pages = page.Split('-', StringSplitOptions.RemoveEmptyEntries);
-                lastPage = int.Parse(pages[1]);
-                firstPage = int.Parse(pages[0]);
-            }
-            else
-            {
-                lastPage = 0;
-                firstPage = int.Parse(page);
-            }
-        }
-        private static void ExportCatalog(InfoBase infoBase, ApplicationObject metaObject, IMetadataService metadata, int pageNumber, int lastPage, string rmq, string route)
-        {
-            string messageType = "Справочник.Клиенты";
-
-            CatalogJsonSerializer serializer = new CatalogJsonSerializer();
-            CatalogDataMapper mapper = new CatalogDataMapper(infoBase, metaObject);
-
-            int count = mapper.GetTotalRowCount(metadata.ConnectionString);
-            Console.WriteLine($"Total rows count = {count}");
-
-            count = 0;
-            for (; pageNumber <= lastPage; pageNumber++)
-            {
-                List<Клиенты> list = mapper.SelectEntities(metadata.ConnectionString, pageNumber);
-
-                foreach (Клиенты entity in list)
+                foreach (string error in errors)
                 {
-                    if (entity.КонтактнаяИнформация == null)
-                    {
-                        entity.КонтактнаяИнформация = mapper.SelectTablePart(metadata.ConnectionString, entity);
-                    }
+                    Console.WriteLine(error);
                 }
-
-                using (RabbitMQProducer producer = new RabbitMQProducer())
-                {
-                    producer.MessageType = messageType;
-                    producer.Publish(rmq, route, list, serializer);
-
-                    Console.WriteLine($"Page [{pageNumber}] = {list.Count}");
-                }
-
-                count += list.Count;
             }
 
-            Console.WriteLine($"A total of {count} messages has been sent");
+            watch.Stop();
+
+            Console.WriteLine($"Created {result} views in {watch.ElapsedMilliseconds} ms");
         }
-        private static void ExportRegister(InfoBase infoBase, ApplicationObject metaObject, IMetadataService metadata, int pageNumber, int lastPage, string rmq, string route)
+        private static void ExecuteDeleteCommand(string ms, string pg)
         {
-            string messageType = "РегистрСведений.ОтветственныеЛицаАптек";
-
-            ClusteredIndexInfo indexInfo = SQLHelper.GetClusteredIndexInfo(metadata.ConnectionString, metaObject.TableName);
-
-            RegisterJsonSerializer serializer = new RegisterJsonSerializer();
-            RegisterDataMapper mapper = new RegisterDataMapper(infoBase, metaObject, indexInfo);
-
-            int count = mapper.GetTotalRowCount(metadata.ConnectionString);
-            Console.WriteLine($"Total rows count = {count}");
-
-            count = 0;
-            for (; pageNumber <= lastPage; pageNumber++)
+            if (!string.IsNullOrWhiteSpace(pg))
             {
-                List<ОтветственныеЛицаАптек> list = mapper.SelectRecords(metadata.ConnectionString, pageNumber);
-
-                using (RabbitMQProducer producer = new RabbitMQProducer())
-                {
-                    producer.MessageType = messageType;
-                    producer.Publish(rmq, route, list, serializer);
-
-                    Console.WriteLine($"Page [{pageNumber}] = {list.Count}");
-                }
-
-                count += list.Count;
+                Console.WriteLine("Sorry, PostgreSql support is under construction ... ");
+                return;
             }
 
-            Console.WriteLine($"A total of {count} messages has been sent");
+            SqlGeneratorOptions options = new SqlGeneratorOptions()
+            {
+                DatabaseProvider = "SqlServer",
+                ConnectionString = ms
+            };
+
+            ISqlGenerator generator = new SqlGenerator(options);
+
+            Stopwatch watch = new();
+
+            watch.Start();
+
+            int result = generator.DropViews();
+
+            watch.Stop();
+
+            Console.WriteLine($"Dropped {result} views in {watch.ElapsedMilliseconds} ms");
         }
     }
 }
