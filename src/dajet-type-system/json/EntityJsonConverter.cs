7284dc62745161c277949a52a1508933f78d1361
zhichkin <dima_zhichkin@mail.ru>
2026-06-29 12:06:00 +0300

    DaJet Type System 1.0.11
    EntityJsonConverter

---
+++ src/dajet-type-system/json/EntityJsonConverter.cs
@@ -0,0 +1,25 @@
+﻿using DaJet.TypeSystem;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DaJet.Json
+{
+    public sealed class EntityJsonConverter : JsonConverter<Entity>
+    {
+        public override void Write(Utf8JsonWriter writer, Entity value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString());
+        }
+        public override Entity Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType != JsonTokenType.String)
+            {
+                throw new FormatException();
+            }
+
+            string value = reader.GetString();
+
+            return Entity.Parse(value);
+        }
+    }
+}
